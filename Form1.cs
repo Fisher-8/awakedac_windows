0175b3915e4ed7bc4f87e2a441c8857b73af263c
Fisher-8 <213799743+Fisher-8@users.noreply.github.com>
2026-09-22 20:12:41 +0900

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace awakedac_windows
+{
+    public partial class Form1 : Form
+    {
+        private NotifyIcon notifyIcon;
+        private ContextMenuStrip contextMenu;
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            this.Visible = false;
+            contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Close", null, (s, a) => this.Close());
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = Properties.Resources.awakedac;
+            notifyIcon.ContextMenuStrip = contextMenu;
+            notifyIcon.Visible = true;
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            base.OnFormClosing(e);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Forms;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace awakedac_windows
+{
+    internal static class Program
+    {
+        private static NAudio.Wave.WasapiOut waveOut;
+        private static NAudio.Wave.SampleProviders.SignalGenerator nullOut;
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(NaudioAssembly);
+            AwakeDAC();
+            Application.ApplicationExit += (s, e) => ReleaseDAC();
+            Application.Run(new Form1());
+
+        }
+        private static Assembly NaudioAssembly(object sender, ResolveEventArgs args)
+        {
+            string assemblyName = new AssemblyName(args.Name).Name;
+            switch (assemblyName) {
+                case "NAudio.Core": return Assembly.Load(Properties.Resources.naudio_core);
+                case "NAudio.Wasapi": return Assembly.Load(Properties.Resources.naudio_wasapi);
+                default: return null;
+            }
+        }
+        private static void AwakeDAC()
+        {
+            using (var deviceEnumerator = new NAudio.CoreAudioApi.MMDeviceEnumerator())
+            {
+                var device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                nullOut = new NAudio.Wave.SampleProviders.SignalGenerator() { Gain = 0 };
+                waveOut = new NAudio.Wave.WasapiOut(device, AudioClientShareMode.Shared, true, 1000);
+                waveOut.Init(nullOut);
+                waveOut.Play();
+            }
+        }
+        private static void ReleaseDAC()
+        {
+            waveOut.Stop();
+            waveOut.Dispose();
+            waveOut = null;
+            nullOut = null;
+        }
+    }
+}
